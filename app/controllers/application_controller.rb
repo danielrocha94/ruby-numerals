600315a588bede4492417295b42dd571ea6a9535
danielrocha94 <danielrocha94@msn.com>
2016-04-14 22:33:44 -0500

    remove useless field in application_controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,9 +5,4 @@ class ApplicationController < ActionController::Base
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
 
-  def create
-    number = params[:input].to_i
-    numeral = Numerals.new
-    result = numeral.number_words()
-  end
 end
